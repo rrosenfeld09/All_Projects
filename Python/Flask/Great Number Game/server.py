75525d568eebd39f2528d217cbcbc02d49ef1542
Richard Rosenfeld <rwrosenfeld@gmail.com>
2018-04-22 17:20:19 -0400

    updated Great Number Game project to correct session data error

--- Python/Flask/Great Number Game/server.py
+++ Python/Flask/Great Number Game/server.py
@@ -7,9 +7,36 @@ app.secret_key = "SuperSecret"
 
 @app.route("/")
 def index():
+	dump_data = False
+	if dump_data:
+		session['random'] = None
+		session['gameover'] = None
+		session['answer'] = None
+
+	if session.get('random') == None:
+		print("NEW SESSION!")
+	else:
+		print("RESUMING SESSION")
+
+	if session.get('gameover') == None:
+		print("GAMEOVER IS ALSO NONE")
+	else:
+		print("GAMEOVER HAS A VALUE")
+		
 	try: 
+		if session.get('gameover') == None:
+			session['gameover'] = True
+		else:
+			print session['gameover']
+
 		if session['gameover'] == True:
+			print("GAME IS OVER, GENERATING NEW RANDOM NUMBER")
 			session['random'] = random.randrange(1, 101)
+			session['answer'] = -1
+		else:
+			print("ELSE STATEMENT")
+			print session['gameover']
+		print("BEGIN GAME")
 	except NameError:
 		x = random.randrange(1,101)
 		session['random'] = x
@@ -20,8 +47,11 @@ def index():
 
 @app.route("/answer", methods = ["POST"])
 def answer():
-	session['gameover'] = False
-	session['answer'] = int(request.form['answer'])
+	if session.get('gameover') == None:
+		print("IGNORING SUBMISSION FOR NONEXISTANT SESSION")
+	else:
+		session['gameover'] = False
+		session['answer'] = int(request.form['answer'])
 	return redirect("/")
 
 @app.route("/reset", methods = ["POST"])
