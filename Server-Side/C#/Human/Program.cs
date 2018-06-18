9b9010fa4357726ae565bd5f302a3deb147f25a3
Richard Rosenfeld <rwrosenfeld@gmail.com>
2018-06-17 22:31:45 -0400

    added Human project to C# directory

---
+++ Server-Side/C#/Human/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human steven = new Human("Steven");
+            Human rob = new Human("Rob");
+
+            steven.attack(rob);
+            rob.attack(steven);
+        }
+    }
+}
