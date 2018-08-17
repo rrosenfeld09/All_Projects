cc40ab14f2d9f6cd67c86d33421606ca3e69e9b6
Richard Rosenfeld <rwrosenfeld@gmail.com>
2018-08-16 23:52:56 -0400

    added LoginRegistration project to C# directory

---
+++ Server-Side/C#/ORM/Entity Framework/LoginRegistration/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using LoginRegistration.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace LoginRegistration
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration {get; private set;}
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserContext>(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
+            services.AddSession();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+            app.UseMvc();
+            app.UseStaticFiles();
+        }
+    }
+}
